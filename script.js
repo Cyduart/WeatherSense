a1ad83bc1c5927297e79837e9014be37355a9d53
Cyduart <futuristcgamez@gmail.com>
2023-07-20 21:22:35 -0300

    implementing functions for the application from the API

--- script.js
+++ script.js
@@ -14,4 +14,8 @@ window.addEventListener("scroll", handleScroll);
 document.querySelector('.busca').addEventListener('submit', (event) => {
   event.preventDefault();
 
+  let input = document.querySelector('#searchInput').value;
+
+  console.log(input);
+
 });
