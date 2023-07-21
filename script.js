24826b05cab3dedc93abe318731d5189faa22e8b
Cyduart <futuristcgamez@gmail.com>
2023-07-21 00:48:56 -0300

    API ADD

--- script.js
+++ script.js
@@ -11,11 +11,59 @@ function handleScroll() {
 
 window.addEventListener("scroll", handleScroll);
 
-document.querySelector('.busca').addEventListener('submit', (event) => {
+document.querySelector('body').addEventListener('submit', async (event) => {
   event.preventDefault();
 
   let input = document.querySelector('#searchInput').value;
 
-  console.log(input);
+  if (input !== '') {
+    clearInfo();
+    showWarning('Carregando....')
 
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(input)}&appid=da89528f3f150e726a7adae47cc2e76f&units=metric&lang=pt_br`;
+
+    
+      let results = await fetch(url);
+      let json = await results.json();
+      
+      
+      if(json.cod === 200){
+        showInfo({
+          name: json.name,
+          country: json.sys.country,
+          temp: json.main.temp,
+          tempIcon: json.weather[0].icon,
+          windSpeed: json.wind.speed,
+          windAngle: json.wind.deg
+        });
+
+
+      } else {
+        clearInfo();
+        showWarning('Não encontramos esta Localização')
+      }
+
+  }
+
+      
 });
+
+function showInfo(json) {
+  showWarning('');
+
+  document.querySelector('.resultado').style.display = 'block';
+  document.querySelector('.titulo').innerHTML = `${json.name}, ${json.country}`;
+  document.querySelector('.tempInfo').innerHTML = `${json.temp} <sup>ºC</sup>`;
+  document.querySelector('.ventoInfo').innerHTML = `${json.windSpeed} <span>km/h</span>`;
+
+}
+function clearInfo(){
+  showWarning('');
+  document.querySelector('.resultado').style.display = 'none';
+
+}
+
+
+function showWarning(msg) {
+  document.querySelector('.aviso').innerHTML = msg;
+}
