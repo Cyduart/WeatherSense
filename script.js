ed093dc21e6a4ef774fe2a8fb67c0f4efbc08da5
Cyduart <futuristcgamez@gmail.com>
2023-07-20 21:16:50 -0300

    New codes/new tags css

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 function handleScroll() {
   const navbar = document.querySelector(".navbar");
   const header = document.querySelector("header");
-
-  // Obtém a altura do header
   const headerHeight = header.getBoundingClientRect().height;
-
-  // Verifica se o scroll da página é maior do que a altura do header
   if (window.scrollY > headerHeight) {
     navbar.classList.add("fixed-navbar");
   } else {
     navbar.classList.remove("fixed-navbar");
   }
 }
+
 window.addEventListener("scroll", handleScroll);
+
+document.querySelector('.busca').addEventListener('submit', (event) => {
+  event.preventDefault();
+
+});
